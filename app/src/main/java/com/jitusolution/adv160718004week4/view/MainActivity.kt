7afea88577ffd1aaef4c4e205f60b32acace6e2b
kharuzan <74102414+kharuzan@users.noreply.github.com>
2021-03-24 16:00:05 +0700

    sampai menit 14 week7

--- app/src/main/java/com/jitusolution/adv160718004week4/view/MainActivity.kt
+++ app/src/main/java/com/jitusolution/adv160718004week4/view/MainActivity.kt
@@ -2,11 +2,33 @@ package com.jitusolution.adv160718004week4.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.jitusolution.adv160718004week4.R
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var observable = Observable.just("hellow","world","!!")
+        val observer = object: Observer<String> {
+            override fun onSubscribe(d: Disposable?) { // memberitahui bahwa observer memulai subscribe
+                Log.d("Messages","Start subscribe")
+            }
+
+            override fun onNext(t: String?) {
+                Log.d("Messages","Start subscribe")
+            }
+
+            override fun onError(e: Throwable?) {
+            }
+
+            override fun onComplete() {
+            }
+
+        }
     }
 }
